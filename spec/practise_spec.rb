6b89276edb9487bc3f138ac01fdfc69cd9869027
joshSteadman <josh.steadman15@gmail.com>
2020-08-05 11:15:36 +0100

    First test made

---
+++ spec/practise_spec.rb
@@ -0,0 +1,7 @@
+require 'practise'
+describe 'array_product' do
+    it "array_product returns 8 when passed array_product([[1,2], [3,4]])" do
+        expect(array_product([[1, 2], [3, 4]])).to eq 8
+   
+    end
+end
