49eed93070469b00d8a47b6c0f0528044561bb6b
joshSteadman <josh.steadman15@gmail.com>
2020-08-05 11:28:47 +0100

    Third test made and failed

--- spec/practise_spec.rb
+++ spec/practise_spec.rb
@@ -6,4 +6,7 @@ describe 'array_product' do
     it "array_product returns 16 when passed array_product([[1,2], [5,8]])" do
       expect(array_product([[1, 2], [5, 8]])).to eq 16
     end
+    it "array_product returns 32 when passed array_product([[1,2], [5,8]])" do
+        expect(array_product([[1, 4], [8, 5]])).to eq 32
+    end
 end
